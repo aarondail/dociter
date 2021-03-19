4bacefae4590b06b027c5c50a9650139e327bf6f
Aaron Dail <aaron@planetdail.com>
2021-03-18 19:49:48 -0700

    Exploring different fonts we could use.

--- explorations/text-rendering-approach/src/App.tsx
+++ explorations/text-rendering-approach/src/App.tsx
@@ -1,23 +1,196 @@
+/* eslint-disable no-irregular-whitespace */
 import React from "react";
 
 import { ThreeJsApp } from "./ThreeJsApp";
 
+const testText =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." +
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." +
+  // "The quick brown fox ate a zesty hamburgerfons .The laughed." +
+  // "The quick brown fox ate a zesty hamburgerfons .The laughed." +
+  // "The quick brown fox ate a zesty hamburgerfons .The laughed." +
+  // "The quick brown fox ate a zesty hamburgerfons .The laughed." +
+  // "The quick brown fox ate a zesty hamburgerfons .The laughed." +
+  // "The quick brown fox ate a zesty hamburgerfons .The laughed." +
+  // "The quick brown fox 🦊 ate a zesty hamburgerfons 🍔.The 👩‍👩‍👧‍👧 laughed." +
+  // "The quick brown fox 🦊 ate a zesty hamburgerfons 🍔.The 👩‍👩‍👧‍👧 laughed." +
+  // "The quick brown fox 🦊 ate a zesty hamburgerfons 🍔.The 👩‍👩‍👧‍👧 laughed." +
+  // "The quick brown fox 🦊 ate a zesty hamburgerfons 🍔.The 👩‍👩‍👧‍👧 laughed." +
+  // "The quick brown fox 🦊 ate a zesty hamburgerfons 🍔.The 👩‍👩‍👧‍👧 laughed." +
+  // "The quick brown fox 🦊 ate a zesty hamburgerfons 🍔.The 👩‍👩‍👧‍👧 laughed." +
+  ""; /*  `
+Ｉ　ａｍ　ｈｅｒｅ　トアイ
+Ｉ░ａｍ░ｈｅｒｅ　（トアイ）
+【﻿Ｉ　ａｍ　ｈｅｒｅ】
+丨　卂爪　卄乇尺乇
+工　丹冊　廾ヨ尺ヨ
+ﾉ　ﾑﾶ　ん乇尺乇
+↓ᵐᵒʳᵉ ᵗᵉˣᵗ ᶠᵒⁿᵗˢ↓
+ℑ 𝔞𝔪 𝔥𝔢𝔯𝔢
+𝕴 𝖆𝖒 𝖍𝖊𝖗𝖊
+★☞  ι αＭ ℍ𝔢гᵉ  🐯💘
+𝓘 𝓪𝓶 𝓱𝓮𝓻𝓮
+𝐼 𝒶𝓂 𝒽𝑒𝓇𝑒
+𝕀 𝕒𝕞 𝕙𝕖𝕣𝕖
+Ｉ ａｍ ｈｅｒｅ
+*  🎀  𝐼 𝒶𝓂 𝒽𝑒𝓇𝑒  🎀  *
+ɪ ᴀᴍ ʜᴇʀᴇ
+ǝɹǝɥ ɯɐ I
+I⃣    a⃣   m⃣    h⃣   e⃣   r⃣   e⃣
+I⃞     a⃞    m⃞     h⃞    e⃞    r⃞    e⃞
+🄸 🄰🄼 🄷🄴🅁🄴
+ɘɿɘʜ mɒ I
+Ȋ̴̟̙̺̜̜̮̝̎̋͐͆͘͜͠ ̴̧̗̃̍͝â̷̢̲̳̔̋̾͆m̴̪͎̝͔͔͕̾̎͗̾̍̒̎͑͝ͅ ̷̡̡̱̘̬̯͖̤̬̲̈̌͆͋͆̉́̿͆ḩ̷̢̰͔̩̲̥̭̪̏e̴̛̳͒̇͆͐͝r̷͔̍͒͋̍̓̋͘͝ë̷̦̟̫̼̞̟̺͍̪́̈́͜͝
+🅸 🅰🅼 🅷🅴🆁🅴
+ᵢ ₐₘ ₕₑᵣₑ
+ᴵ ᵃᵐ ʰᵉʳᵉ
+Ⓘ ⓐⓜ ⓗⓔⓡⓔ
+เ ค๓ ђєгє
+i ค๓ hērē
+𝐈 𝐚𝐦 𝐡𝐞𝐫𝐞
+𝘐 𝘢𝘮 𝘩𝘦𝘳𝘦
+𝙄 𝙖𝙢 𝙝𝙚𝙧𝙚
+𝙸 𝚊𝚖 𝚑𝚎𝚛𝚎
+I ΛM ΉΣЯΣ
+ι αм нєяє
+Ì åm hêrê
+ł ₳₥ ⱧɆⱤɆ
+【I】 【a】【m】 【h】【e】【r】【e】
+『I』 『a』『m』 『h』『e』『r』『e』
+≋I≋ ≋a≋m≋ ≋h≋e≋r≋e≋
+░I░ ░a░m░ ░h░e░r░e░
+(っ◔◡◔)っ ♥ I am here ♥
+˜”*°•.˜”*°• I am here •°*”˜.•°*”˜
+[̲̅I] [̲̅a][̲̅m] [̲̅h][̲̅e][̲̅r][̲̅e]
+I҉ ҉a҉m҉ ҉h҉e҉r҉e҉
+į ąʍ հҽɾҽ
+I ᗩᗰ ᕼEᖇE
+ᓰ ᗩᘻ ᕼᘿᖇᘿ
+I̶ ̶a̶m̶ ̶h̶e̶r̶e̶
+I̴ ̴a̴m̴ ̴h̴e̴r̴e̴
+I̷ ̷a̷m̷ ̷h̷e̷r̷e̷
+I̲ ̲a̲m̲ ̲h̲e̲r̲e̲
+I̳ ̳a̳m̳ ̳h̳e̳r̳e̳
+I̾ ̾a̾m̾ ̾h̾e̾r̾e̾
+I♥ ♥a♥m♥ ♥h♥e♥r♥e
+I͎ ͎a͎m͎ ͎h͎e͎r͎e͎
+
+I͓̽ ͓̽a͓̽m͓̽ ͓̽h͓̽e͓̽r͓̽e͓̽
+Wingdings: ✋︎ ♋︎❍︎ ♒︎♏︎❒︎♏︎
+･ﾟ･ﾟ  🎀  𝐼 𝒶𝓂 𝒽𝑒𝓇𝑒  🎀  ﾟ･ﾟ･
+🍓 ⋆ 🎂  🎀  𝐼 𝒶𝓂 𝒽𝑒𝓇𝑒  🎀  🎂 ⋆ 🍓
+]|I{•------»   🎀  𝐼 𝒶𝓂 𝒽𝑒𝓇𝑒  🎀   »------•{I|]
+🍇 ⋆ 🍉  🎀  𝐼 𝒶𝓂 𝒽𝑒𝓇𝑒  🎀  🍉 ⋆ 🍇
+🍎 ⋆ 🍑  🎀  𝐼 𝒶𝓂 𝒽𝑒𝓇𝑒  🎀  🍑 ⋆ 🍎
+💲💙  𝔦 ΔΜ 卄єℝ𝑒  🐉♕
+🐼👹  𝓲 ᵃᵐ 𝔥𝓔ℝ𝐄  ★✌
+😂♦  Į 𝕒Ⓜ Ĥєг𝐞  ♤☆
+☺♦  𝐢 𝒶爪 ᕼ𝕖я乇  ✌🐍
+👍😳  Ɨ αⓜ 𝐇ᗴʳＥ  💋💎
+🎅💋  𝓘 άⓜ ｈⓔ𝐑乇  💙♝
+➶➶➶➶➶ ί 𝒶м Ĥ𝔢𝓇𝐞 ➷➷➷➷➷
+💞🐣  𝕚 α𝕞 Ｈ𝑒𝐫ᵉ  🐧🍓
+💚✌  𝓲 𝐀ｍ 𝕙𝔢𝕣є  ♛🐠
+💋♤  ι Ａ𝐌 𝓱ε𝐫Ｅ  🐍😂
+🍪🐚  Ｉ Δ爪 ʰＥяẸ  🐲💔
+✌👺  Ⓘ άⓜ ｈᵉⓇ𝔢  🍪♡
+🐟🐨  𝐢 α๓ ⓗᗴʳє  🍪🐻
+🐨🍟  ί ᵃⓜ ʰⒺяᵉ  💢🍧
+💛💣  Ꭵ 𝔞ｍ Ĥєᖇⓔ  ✋🐙
+💣♕  Ɨ Δ𝐦 ђ€𝓡ε  🐙♜
+✎💚  ｉ 𝓪м ђέ𝐫𝑒  💢🐼
+☝🍭  𝐢 𝓐𝓶 Ｈ𝕖ᖇｅ  👍👑
+🐣💔  𝔦 ａϻ ⓗᗴʳ𝐄  🎁🎄
+🐔  🎀  𝐼 𝒶𝓂 𝒽𝑒𝓇𝑒  🎀  🐔
+🐎  🎀  𝐼 𝒶𝓂 𝒽𝑒𝓇𝑒  🎀  🐎
+✩  🎀  𝐼 𝒶𝓂 𝒽𝑒𝓇𝑒  🎀  ✩
+`;
+*/
 function App(): JSX.Element {
   const canvasRef = React.useRef<HTMLCanvasElement | null>(null);
   const threeJsContainerRef = React.useRef<ThreeJsApp | null>(null);
 
-  const setCanvas = React.useCallback((e: HTMLCanvasElement | null) => {
-    if (threeJsContainerRef.current) {
-      threeJsContainerRef.current.dispose();
-      threeJsContainerRef.current = null;
-    }
-    canvasRef.current = e;
-    if (canvasRef.current) {
-      threeJsContainerRef.current = new ThreeJsApp(canvasRef.current);
-    }
-  }, []);
+  // const setCanvas = React.useCallback((e: HTMLCanvasElement | null) => {
+  //   if (threeJsContainerRef.current) {
+  //     threeJsContainerRef.current.dispose();
+  //     threeJsContainerRef.current = null;
+  //   }
+  //   canvasRef.current = e;
+  //   if (canvasRef.current) {
+  //     threeJsContainerRef.current = new ThreeJsApp(canvasRef.current);
+  //   }
+  // }, []);
 
-  return <canvas id="canvas" ref={setCanvas} />;
+  // return <canvas id="canvas" ref={setCanvas} />;
+
+  // const html = React.useMemo(() => {
+  //   return testText;
+  // }, [testText]);
+  const elements = React.useMemo(() => {
+    const result = [];
+    let li = 0;
+    for (const line of testText.split("\n")) {
+      li++;
+      const cp = 0;
+      // for (const codePoint of [...line]) {
+      //   cp++;
+      //   result.push(<span key={`${li}-${cp}`}>{codePoint}</span>);
+      // }
+      // result.push(<br key={`${li}-${cp}`} />);
+      result.push(<span key={`${li}-span`}>{line}</span>);
+      result.push(<br key={`${li}-${cp}-b`} />);
+    }
+    return result;
+  }, [testText]);
+
+  // HMMMM
+  // https://developer.mozilla.org/en-US/docs/Web/API/Range
+  // https://jsfiddle.net/abrady0/ggr5mu7o/
+  // https://stackoverflow.com/questions/5143534/how-to-get-the-position-of-text-within-an-element
+  // https://earwicker.com/carota/
+  // https://practicaltypography.com/hyphenation.html
+  // https://stackoverflow.com/questions/32701210/how-to-control-the-font-kerning-for-canvas-filltext-in-chrome
+  // https://github.com/basecamp/trix
+  // https://bboxtype.com/typefaces/FiraSans/#!layout=specimen
+
+  const commonStyle: React.CSSProperties = {
+    fontSize: "16px",
+    // color: "white",
+    // fontWeight: 600,
+    WebkitHyphens: "auto",
+    textRendering: "optimizeLegibility",
+    // letterSpacing: "0.05em",
+    // fontFamily: "'Fira Sans', sans-serif",
+    // fontFamily: "'Fira Sans Condensed', sans-serif",
+    fontFamily: "'Source Serif', sans-serif",
+    fontKerning: "normal",
+    fontFeatureSettings: '"kern" 1',
+  };
+  return (
+    // <div style={{ width: "100%", height: "100%", overflow: "scroll" }} dangerouslySetInnerHTML={{ __html: html }}></div>
+    <div style={{ width: "100%", height: "100%", backgroundColor: "#e1eef1", padding: 40 }}>
+      <div style={{ ...commonStyle, fontFamily: "'Fira Sans'" }}>{elements}</div>
+      <br />
+      {/* <div style={{ ...commonStyle, fontFamily: "'Fira Sans Condensed'" }}>{elements}</div>
+      <br />
+      <div style={{ ...commonStyle, fontFamily: "'Fira Sans Compressed'" }}>{elements}</div>
+      <br /> */}
+      <div style={{ ...commonStyle, fontFamily: "'Source Serif SmText'" }}>{elements}</div>
+      <br />
+      <div style={{ ...commonStyle, fontSize: "17.5px", lineHeight: "1.35em" }}>{elements}</div>
+      {/* <br />
+      <div style={{ ...commonStyle, fontWeight: 600 }}>{elements}</div>
+      <br />
+      <div style={{ ...commonStyle, fontWeight: 700 }}>{elements}</div> */}
+      {/* <br />
+      <div style={{ ...commonStyle, fontFamily: "'Source Serif Caption'" }}>{elements}</div> */}
+      <br />
+      <div style={{ ...commonStyle, fontFamily: "'Source Serif Display'" }}>{elements}</div>
+      <br />
+      <div style={{ ...commonStyle, fontFamily: "'Source Serif Subhead'" }}>{elements}</div>
+      <br />
+    </div>
+  );
 }
 
 export default App;
