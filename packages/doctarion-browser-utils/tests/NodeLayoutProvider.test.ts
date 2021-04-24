5de7c97a1a2a6490a45762471a8f30befea04e8a
Aaron Dail <aaron@planetdail.com>
2021-04-24 12:08:26 -0700

    Updating test to try to do something.

--- packages/doctarion-browser-utils/tests/NodeLayoutProvider.test.ts
+++ packages/doctarion-browser-utils/tests/NodeLayoutProvider.test.ts
@@ -1,26 +1,33 @@
-describe("NodeLayoutProvider", () => {
-  // afterEach(() => {
-  //   const kids = [];
-  //   for (let i = 0; i < document.body.children.length; i++) {
-  //     const c = document.body.children[i];
-  //     kids.push(c);
-  //   }
-  //   kids.forEach((c) => document.body.removeChild(c));
-  // });
+import { InlineText, Text } from "doctarion-document";
 
-  it("it works", () => {
+import { NodeLayoutProvider } from "../src";
+
+describe("NodeLayoutProvider", () => {
+  describe("one line with no wrapping", () => {
+    const node = InlineText.new("I am a little pony");
+    let el: HTMLElement;
+    let p: NodeLayoutProvider;
+    beforeEach(() => {
+      el = document.createElement("p");
+      el.style.cssText = "width: 200px; background-color: #999; font-size: 20px; font-family: Arial;";
+      el.textContent = Text.toString(node.text);
+      document.body.appendChild(el);
+      p = new NodeLayoutProvider(el, node);
+    });
     // console.log(electron);
     // console.log(electron.remote);
     // electron.remote.getCurrentWindow().show();
 
-    const el = document.createElement("p");
-    el.style.cssText = "width: 200px; background-color: #999; font-size: 20px; font-family: Arial;";
-    el.textContent = "I am a little pony.";
-    document.body.appendChild(el);
+    // This is really just to make sure things get layed out correctly
+    it("the element gets added and doesn't wrap", () => {
+      expect(el.getClientRects().length).toEqual(1);
+    });
 
-    for (let i = 0; i < el.getClientRects().length; i++) {
-      console.log(JSON.stringify(el.getClientRects()[i]));
-    }
-    expect(1).toEqual(1);
+    it("no line breaks are detected", () => {
+      const layout = p.getDetailedLayoutForNodeContainingOnlyText();
+      expect(layout).toBeDefined();
+      expect(layout?.layoutRects.length).toBe(1);
+      expect(layout?.graphemeLineBreaks?.size).toBe(0);
+    });
   });
 });
