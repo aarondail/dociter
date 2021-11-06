385ecf750fb4000448499a9e8067e1c8c114c69f
Aaron Dail <aaron@planetdail.com>
2021-11-05 20:58:52 -0700

    Porting over cursorOps tests to cursorCommands

---
+++ packages/doctarion-document/tests/commands-rd4/commands.testUtils.ts
@@ -0,0 +1,21 @@
+/* eslint-disable jest/no-export */
+import { Commands } from "../../src/commands-rd4";
+import { Editor } from "../../src/editor-rd4";
+import { testDoc } from "../utils-rd4";
+
+export const CommandsTestUtils = {
+  testDocs: {
+    basicDoc: testDoc`
+<h level=ONE> <s>Header1</s> </h>
+<p> <s styles=6:+B>MMNNAABB</s> </p>
+<p> </p>
+<p> <s>CC</s> <hyperlink url=g.com>GOOGLE</hyperlink> <s>DD</s> </p>
+`,
+  },
+
+  getEditorForBasicDoc(): Editor {
+    const editor = new Editor({ document: CommandsTestUtils.testDocs.basicDoc });
+    editor.execute(Commands.updateInteractor({ id: editor.state.focusedInteractor!.id, name: "᯼" }));
+    return editor;
+  },
+};
